40dc1a29ddab1cdbf19d8faf5ad5d25438477d2a
Sotonye Orifama <109242468+sotoriz@users.noreply.github.com>
2023-06-09 16:36:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # define a route and function to handle the request
-@app.route('/landing.html')
+#@app.route('/landing.html')
 def index():
     # render the HTML file using the Jinja templating engine
-    return render_template('landing.html')
+  #  return render_template('landing.html')
 
 # start the app
 if __name__ == '__main__':
